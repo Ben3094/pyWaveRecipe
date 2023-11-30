7540a471258e3f10bfa39011088bb21dda4dce4c
Benjamin SAGGIN <benjamin.saggin@live.fr>
2023-11-30 15:40:06 +0100

    [Fix] Installation

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt", "r", encoding="utf-16") as fh:
 
 setup(
     name='pyWaveRecipe',
-    version='0.0.18',
+    version='0.0.23',
     author='Benjamin SAGGIN',
     description='Tools for storing S-parameters from electromagnetic components and combine them while preserving external dependencies of each components',
     long_description=long_description,
@@ -18,6 +18,6 @@ setup(
         "Bug Tracker": ""
     },
     license='MIT',
-    packages=find_packages(where="src"),
+    packages=find_packages(),
     install_requires=install_requires,
 )
