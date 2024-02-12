c0153e8eea4949e885915576cb1809d3586db833
Benjamin SAGGIN <benjamin.saggin@live.fr>
2024-02-12 15:03:12 +0100

    [Fix] modules advertising

---
+++ pyWaveRecipe/__init__.py
@@ -0,0 +1,2 @@
+from .Circuit import Circuit, COMPONENT_PROPERTY_NAME, PORT_CONNECTION_PROPERTY_NAME
+from .Component import Component, FREQUENCY_HEADER, MAX_POWERS_HEADER
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt", "r", encoding="utf-8") as fh:
 
 setup(
     name='pyWaveRecipe',
-    version='0.0.38',
+    version='0.0.40',
     author='Benjamin SAGGIN',
     description='Tools for storing S-parameters from electromagnetic components and combine them while preserving external dependencies of each components',
     long_description=long_description,
