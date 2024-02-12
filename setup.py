8ae5b9d43e4063410e064ad5f6c34a088d2cda18
Benjamin SAGGIN <benjamin.saggin@live.fr>
2024-02-12 14:54:39 +0100

    [Fix] setup.py codec

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup, find_packages
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-with open("requirements.txt", "r", encoding="utf-16") as fh:
+with open("requirements.txt", "r", encoding="utf-8") as fh:
     install_requires = [line.strip() for line in fh.readlines()]
 
 setup(
     name='pyWaveRecipe',
-    version='0.0.37',
+    version='0.0.38',
     author='Benjamin SAGGIN',
     description='Tools for storing S-parameters from electromagnetic components and combine them while preserving external dependencies of each components',
     long_description=long_description,
